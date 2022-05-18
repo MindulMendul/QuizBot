e67e6342435eb6fb790b8dc28e78d780cf9d5090
mindul <pencake33@naver.com>
2022-05-19 00:11:14 +0900

    컴포넌트화

--- bot.js
+++ bot.js
@@ -1,3 +1,5 @@
+const quiz=require("./src/cmd/quiz");
+
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client({
@@ -11,19 +13,31 @@ const bot = new Discord.Client({
 	partials: ['CHANNEL',]});
 exports.bot=bot;//봇
 
+const commands = new Discord.Collection(); //명령어 모음집
 bot.on('ready', async (a) => {//정상적으로 작동하는지 출력하는 코드
     console.log(`${bot.user.tag}님이 로그인했습니다.`);
     bot.user.setActivity(process.env.activityString, { type: 'PLAYING' });
+
+	commands.set(quiz.name, quiz.execute);
 });
 
 bot.on('messageCreate', async (msg) => {
     if(msg.author.bot) return;//봇은 거름
 	if(msg.channel.type==="DM") return msg.channel.send("DM은 막혀있어요, 죄송합니다. ㅠㅠ");
 	
+	if(!msg.content.startsWith(process.env.PREFIX)) return; //PREFIX 확인
+	
 	const args = msg.content.slice(process.env.PREFIX.length).trim().split(/\s+/);//명령어 말 배열에 담기
 	const command = args.shift();//명령어 인식할 거
-
-	msg.channel.send(command);
+	
+	try{
+		const exe=commands.get(command); // execute 저장
+		if(exe===undefined) return; // 없으면 스킵
+		exe(msg); // 있으면 ㄱㄱ
+	} catch (err) {
+		console.error(err);
+	}
+	
 });
 
 bot.login(process.env.BOT_TOKEN);
--- src/cmd/quiz.js
+++ src/cmd/quiz.js
@@ -1,7 +1,6 @@
 const {MessageActionRow, MessageButton}=require('discord.js');
-module.exports = {
+module.exports={
 	name: `퀴즈`,
-	cmd: ["퀴즈"],
     permission: ["ADD_REACTIONS", "EMBED_LINKS"],
     //타로하트 생성과정
     async execute(msg){
@@ -24,21 +23,8 @@ module.exports = {
         const filter = () => {return true;}
         const collector = asdf.createMessageComponentCollector({filter});
         collector.on('collect', async i => {
-            const tarotEmbed = {
-                color: 0xF7CAC9,
-                author: {
-                    name: '민둘봇의 타로 하트',
-                    icon_url: 'https://i.imgur.com/AD91Z6z.jpg',
-                },
-                description: `${strDes}`,
-                fields:[{name: `오늘은 **${strField[0]}**이에요`, value: strField[2]}],
-                image: {url: strField[1]},//url말고 다른 방법이 없을까
-                footer: {
-                    text: `모든 설명은 심리학 이론인 바넘효과를 바탕으로 작성되었습니다.`,
-                    icon_url: 'https://i.imgur.com/AD91Z6z.jpg',
-                },
-            };
-            i.update({embeds: [tarotEmbed], components:[]});
+            
+            i.update({content:"ㅎㅇ", embeds: [], components:[]});
         });
     }
 }
