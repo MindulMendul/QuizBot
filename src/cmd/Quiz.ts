b765cee75dccb04f75d9e015a99ef81d4888c2fc
mindul <pencake33@naver.com>
2022-05-29 23:23:12 +0900

    count list 추가

--- src/cmd/Quiz.ts
+++ src/cmd/Quiz.ts
@@ -1,4 +1,4 @@
-import { Message, MessageActionRow, MessageButton } from 'discord.js';
+import { GuildMember, Message, MessageActionRow, MessageButton } from 'discord.js';
 import { cmd, embed, participant } from '../types/type';
 import fs from 'fs';
 import { dirUserDB } from '../../bot';
@@ -25,8 +25,9 @@ export const quiz: cmd = {
     );
 
     const makeQuizStr=(OUserList:Array<string>, XUserList:Array<string>, countNum:number)=>{
-      return`**O를 선택한 사람**\n> ${OUserList}\n**X를 선택한 사람**\n> ${XUserList}\n**OX를 고른 사람** : ${countNum}명`;
+      return`**O를 선택한 사람**\n> ${OUserList.join(" ")} \n**X를 선택한 사람**\n> ${XUserList.join(" ")} \n**OX를 고른 사람 수** : ${countNum}명`;
     }
+
     const quizStr=makeQuizStr([],[],0);
     const asdf = await msg.channel.send({
       content: quizStr,
@@ -47,15 +48,43 @@ export const quiz: cmd = {
     
     collector.on('collect', async (i) => {
       const content = i.message.content;
+      const memberID = (i.member as GuildMember).user.id;
+
       const [O, OCountStr, X, XCountStr, countStr] = content.split("\n");
       
-      const OCount=OCountStr.slice(3).split(", ");
-      const XCount=XCountStr.slice(3).split(", ");
-      const count=Number(countStr.replace(/[^0-9]/g,""));
+      const OCount=OCountStr.slice(3).split(" ");
+      const XCount=XCountStr.slice(3).split(" ");
+      let count=Number(countStr.replace(/[^0-9]/g, "")); 
+      OCount.pop(); XCount.pop();
 
-      console.log(OCount);
-      console.log(XCount);
-      console.log(count);
+      //read UserDB
+      const rawDB = fs.readFileSync(dirUserDB, 'utf8');
+      const DB = JSON.parse(rawDB) as Array<participant>;
+      const validation = DB.find((e: participant) => {
+        return e.id === memberID;
+      }); // entity가 DB 안에 있는지 검사
+      
+      if(validation){
+        const name=validation.name;
+
+        console.log(OCount);
+        console.log(XCount);
+        //Count
+        const OIndex = OCount.indexOf(name);
+        const XIndex = XCount.indexOf(name);
+        console.log(OIndex);
+        console.log(XIndex);
+        if(!OCount.includes(name) && !XCount.includes(name)) count++;
+        
+        //OX list
+        if(OIndex>-1){
+          if (i.customId=="O") OCount[OIndex] = (name);
+          else if (i.customId=="X") XCount[XIndex] = (name);
+        } else {
+          if (i.customId=="O") OCount.push(name);
+          else if (i.customId=="X") XCount.push(name);
+        }        
+      }
 
       i.update({content:makeQuizStr(OCount, XCount, count)});
     });
